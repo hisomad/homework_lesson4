df298dc294a5240bfb36eacd266fe568df9eac72
Vika <vika.tulnikova@bk.ru>
2023-01-19 15:13:07 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите количество элементов массива: ");
+    
+int elementsCount = int.Parse(Console.ReadLine());
+int[] array = new int[elementsCount];
+
+ for (int i = 0; i < array.Length; i++)
+ {
+     Console.Write($"\nВведите элемент массива под индексом {i}: ");
+     array[i] = int.Parse(Console.ReadLine());
+ }
+ 
+Console.Write("[");
+Console.Write(string.Join(", ", array));
+Console.WriteLine("]");
+
