f5ab3965fb8b08ae178bd8f3d5184c19a4fb3572
Vika <vika.tulnikova@bk.ru>
2023-01-19 14:43:16 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+    step = step * a;
+}
+
+Console.WriteLine("A в степени B = " + step);
