7915bbba74dc459110c54ddd8563c4a53d768ab5
Vika <vika.tulnikova@bk.ru>
2023-01-19 15:10:46 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+
+Console.WriteLine("Сумма цифр в числе = " + sum);
